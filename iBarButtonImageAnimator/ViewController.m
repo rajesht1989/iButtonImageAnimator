8767e062ffaa6961650504f44ebef1480e31e505
Rajesh <rajesht1989@gmail.com>
2015-04-07 06:24:29 +0530

    Bar button item animation example added

--- iBarButtonImageAnimator/ViewController.m
+++ iBarButtonImageAnimator/ViewController.m
@@ -20,7 +20,18 @@
     [button setBackgroundImage:[UIImage imageNamed:@"info"] forState:UIControlStateNormal];
     [button setSelectionImage:[UIImage imageNamed:@"info"]];
     [button addTarget:self action:@selector(infoTapped:) forControlEvents:UIControlEventTouchUpInside];
+    [button setClipsToBounds:YES];
     [self.view addSubview:button];
+    
+    
+    button = [[iBAButton alloc] initWithFrame:CGRectMake(100, 100, 25, 30)];
+    [button setBackgroundImage:[UIImage imageNamed:@"info"] forState:UIControlStateNormal];
+    [button setSelectionImage:[UIImage imageNamed:@"itunes"]];
+    [button addTarget:self action:@selector(infoTapped:) forControlEvents:UIControlEventTouchUpInside];
+    
+    UIBarButtonItem *barbtn = [[UIBarButtonItem alloc] initWithCustomView:button];
+    [self.toolbar setItems:[NSArray arrayWithObject:barbtn]];
+
 }
 
 - (void)infoTapped:(UIButton *)button
